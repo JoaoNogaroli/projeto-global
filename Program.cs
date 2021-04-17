d38bb5bff992a9e86df987ff3fea0390351a13ba
JoaoNogaroli <victhornogaroli@gmail.com>
2021-04-17 16:27:06 -0300

    Resultado_final

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace projeto_global
+{
+    class Program
+    {
+        
+
+    public static void Main(String[] args)
+    {
+        var limiteprocurar = 1000000000;
+        var numeros = new int[] {16,9,5,7,11,13,17,19};
+        var mmc = func_mmc(limiteprocurar, numeros);
+
+        Console.WriteLine($" O MMC é: {mmc}");
+    }
+
+    private static int func_mmc(int limiteprocurar, params int[] numeros)
+    {
+        var defaultValue = -1;
+        var maxInteger = numeros.Max();
+
+        for(int multiple =maxInteger; multiple< limiteprocurar; multiple++)
+        {
+            var isMultiple = true;
+            foreach(var integer in numeros)
+            {
+                if(multiple % integer != 0)
+                {
+                    isMultiple = false;
+                }
+            }
+            if(isMultiple)
+            {
+                return multiple;
+            }
+           
+        }
+        return defaultValue;
+    }
+    }
+}
